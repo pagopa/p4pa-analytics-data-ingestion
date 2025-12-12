2e2275b630004fbd9367f099781e35b875c8f3d5
antocalo <antonio.calo@nttdata.com>
2025-12-12 12:09:18 +0100

    update spring version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.jk1.license.filter.*
 
 plugins {
   java
-  id("org.springframework.boot") version "3.5.6"
+  id("org.springframework.boot") version "3.5.8"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "6.3.1.5724"
