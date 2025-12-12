2e20183c59924207de0a3cc37a1cb444478efcad
antocalo <antonio.calo@nttdata.com>
2025-12-12 11:10:02 +0100

    P4ADEV-4314-ingestion-and-export-event-to-raw

--- src/main/java/it/gov/pagopa/analytics/ingestion/event/dataevents/DataEventsConsumer.java
+++ src/main/java/it/gov/pagopa/analytics/ingestion/event/dataevents/DataEventsConsumer.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.analytics.ingestion.event.dataevents.dto.*;
 import it.gov.pagopa.analytics.ingestion.exception.custom.WorkflowInternalErrorException;
 import it.gov.pagopa.analytics.ingestion.model.AssessmentsClassification;
+import it.gov.pagopa.analytics.ingestion.model.ExportFile;
+import it.gov.pagopa.analytics.ingestion.model.Ingestion;
 import it.gov.pagopa.analytics.ingestion.repository.AssessmentsClassificationRepository;
+import it.gov.pagopa.analytics.ingestion.repository.ExportFileRepository;
+import it.gov.pagopa.analytics.ingestion.repository.IngestionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +23,14 @@ import static it.gov.pagopa.analytics.ingestion.event.dataevents.enums.DataEvent
 public class DataEventsConsumer implements Consumer<String> {
 
   private final AssessmentsClassificationRepository assessmentsClassificationRepository;
+  private final IngestionRepository ingestionRepository;
+  private final ExportFileRepository exportFileRepository;
   private final ObjectMapper objectMapper;
 
-  public DataEventsConsumer(AssessmentsClassificationRepository assessmentsClassificationRepository, ObjectMapper objectMapper) {
+  public DataEventsConsumer(AssessmentsClassificationRepository assessmentsClassificationRepository, IngestionRepository ingestionRepository, ExportFileRepository exportFileRepository, ObjectMapper objectMapper) {
     this.assessmentsClassificationRepository = assessmentsClassificationRepository;
+    this.ingestionRepository = ingestionRepository;
+    this.exportFileRepository = exportFileRepository;
     this.objectMapper = objectMapper;
   }
 
@@ -32,8 +40,10 @@ public class DataEventsConsumer implements Consumer<String> {
       DataEventDTO paymentEventDTO = objectMapper.readValue(rawJsonEvent, DataEventDTO.class);
       if (INGESTION.equals(paymentEventDTO.getEventType())) {
         log.info("DataEvent received: {} occurred on IngestionFlowFile having eventId {}", paymentEventDTO.getEventType(), paymentEventDTO.getEventId());
+        saveIngestionEvent(rawJsonEvent);
       } else if (EXPORT_FILE.equals(paymentEventDTO.getEventType())) {
         log.info("DataEvent received: {} occurred on ExportFile having eventId {}", paymentEventDTO.getEventType(), paymentEventDTO.getEventId());
+        saveExportFileEvent(rawJsonEvent);
       } else if (ASSESSMENTS_CLASSIFICATION.equals(paymentEventDTO.getEventType())) {
         log.info("DataEvent received: {} occurred on AssessmentsClassification having eventId {}", paymentEventDTO.getEventType(), paymentEventDTO.getEventId());
         saveAssessmentsClassificationEvent(rawJsonEvent);
@@ -49,6 +59,18 @@ public class DataEventsConsumer implements Consumer<String> {
     assessmentsClassificationRepository.save(
       AssessmentsClassification.builder()
       .assessmentClassificationPayload(assessmentDataDTO)
+      .build());
+  }
+
+  private void saveIngestionEvent(String ingestionData) {
+    ingestionRepository.save(Ingestion.builder()
+      .ingestionPayload(ingestionData)
+      .build());
+  }
+
+  private void saveExportFileEvent(String exportFileData) {
+    exportFileRepository.save(ExportFile.builder()
+      .exportFilePayload(exportFileData)
       .build());
   }
 
